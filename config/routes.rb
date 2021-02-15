968dd6134425cb01d7d74ac60c39ee3cbf98809a
HentaiUsagi <miracle121790@gmail.com>
2021-02-16 02:31:17 +0700

    Add User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'hello/index'
   get 'posts/index' => 'posts#index'
   resources :posts
+  resources :user_profile
 end
---
+++ spec/requests/user_profile_request_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+# RSpec.describe 'UserProfiles', type: :request do
+# end
