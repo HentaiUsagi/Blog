d184a253985172350195ff2feacbadb97b1ba094
KeplerIogan <maksimsysoew@mail.ru>
2021-02-18 15:10:51 +0700

    Rubocop fix tests

--- spec/features/account_action_spec.rb
+++ spec/features/account_action_spec.rb
@@ -1,34 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Account Action', type: :feature do
- describe 'User registration' do
-    it 'returns http success' do
+  describe 'User registration' do
+    let(:r_fill) do
       visit '/users/sign_up'
       fill_in 'user_email', with: 'user@mail.com'
       fill_in 'user_password', with: '12345'
       fill_in 'user_email', with: '12345'
       click_button 'commit'
+    end
+
+    it 'complete registration' do
+      r_fill
       expect(page).to have_current_path('/users')
     end
   end
 
- describe 'Create post' do
-    it 'returns http success' do
+  describe 'Create post' do
+    let(:r_fill) do
       visit '/users/sign_up'
       fill_in 'user_email', with: 'user@mail.com'
       fill_in 'user_password', with: '12345'
       fill_in 'user_email', with: '12345'
       click_button 'commit'
+    end
+    let(:c_post) do
       visit '/posts/new'
       fill_in 'post_title', with: 'New'
       fill_in 'post_summary', with: 'Post'
       fill_in 'post_body', with: 'Create'
       click_on 'Save'
-      expect(page).to have_current_path('/posts/1')
+    end
 
+    it 'complete firs post' do
+      r_fill
+      c_post
+      expect(page).to have_current_path('/posts/1')
     end
   end
-
-
-
 end
--- spec/requests/user_profile_request_spec.rb
+++ spec/requests/user_profile_request_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 require 'support/factory_bot'
 
 RSpec.describe 'UserProfilesController', type: :request do
-    describe 'GET sugn_up' do
-        it 'user.set_user' do
-            get '/users/sign_up'
-            expect(response).to have_http_status(:success)
-        end
+  describe 'GET sugn_up' do
+    it 'user.set_user' do
+      get '/users/sign_up'
+      expect(response).to have_http_status(:success)
     end
+  end
 
-    describe 'GET sign_in' do
-        it 'user.set_user' do
-            get '/users/sign_in'
-            expect(response).to have_http_status(:success)
-        end
+  describe 'GET sign_in' do
+    it 'user.set_user' do
+      get '/users/sign_in'
+      expect(response).to have_http_status(:success)
     end
+  end
 end
